4f39d8b12d28c75aa476ff8b6e6db2dac1ec6419
bishal giri <giribishal09@gmail.com>
2020-02-20 10:30:37 +0545

    navbar and header portion completed, simple routing is also done

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from '../pages/homepage/homepage.component';
 import ShopPage from '../pages/shoppage/Shop.component';
+import Header from '../components/header/Header.component';
 import './app.styles.scss';
 
 const HatsPage = () =>(
@@ -12,8 +13,11 @@ const HatsPage = () =>(
 const App = () => {
 	return (
 		<div>
+		<Header />
+		<Switch>
 			<Route exact path='/' component={HomePage} />
 			<Route exact path='/shop' component={ShopPage} />
+		</Switch>
 		</div>
 	)
 }
